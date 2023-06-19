10d7151e512b0cc07f61799202eef7414b4b0d88
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-19 09:16:14 -0300

    Initial commit

---
+++ src/main/java/tech/noetzold/helpout/HelpoutApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.helpout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/noetzold/helpout/HelpoutApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.helpout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
