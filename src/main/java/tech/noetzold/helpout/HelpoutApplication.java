8807b6ce51873e9de49cf4b74724b6c75dbbee55
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-07-10 08:41:31 -0300

    Update HelpoutApplication.java

--- src/main/java/tech/noetzold/helpout/HelpoutApplication.java
+++ src/main/java/tech/noetzold/helpout/HelpoutApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelpoutApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelpoutApplication.class, args);
 	}
 
