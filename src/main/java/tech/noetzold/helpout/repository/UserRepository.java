ef3f590174a45afafe41bee07043735a9e97c233
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-07-30 10:10:47 -0300

    Update UserRepository.java

--- src/main/java/tech/noetzold/helpout/repository/UserRepository.java
+++ src/main/java/tech/noetzold/helpout/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package tech.noetzold.helpout.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.noetzold.helpout.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    public Optional<User> findByLogin(String login);
+
 }
