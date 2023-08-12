e736c453b7f25e66b486fe3f59f1ddfd14d30c98
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-12 06:15:45 -0300

    Update News.java

--- src/main/java/tech/noetzold/helpout/model/News.java
+++ src/main/java/tech/noetzold/helpout/model/News.java
@@ -1,7 +1,13 @@
 package tech.noetzold.helpout.model;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class News {
 }
