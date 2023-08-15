60bb4b07b718693d61ea5d9ba2dacedc3e3acdf8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-15 18:42:00 -0300

    Create DolarValue.java

---
+++ src/main/java/tech/noetzold/helpout/model/DolarValue.java
@@ -0,0 +1,11 @@
+package tech.noetzold.helpout.model;
+
+import java.math.BigDecimal;
+
+public class DolarValue {
+    private BigDecimal ask;
+
+    public BigDecimal getAsk() {
+        return ask;
+    }
+}
