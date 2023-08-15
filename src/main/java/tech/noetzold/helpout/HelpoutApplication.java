db3f75bba1676f6c469666a77c31cc2d18969cdd
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-15 20:38:23 -0300

    Update HelpoutApplication.java

--- src/main/java/tech/noetzold/helpout/HelpoutApplication.java
+++ src/main/java/tech/noetzold/helpout/HelpoutApplication.java
@@ -2,15 +2,23 @@ package tech.noetzold.helpout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients
 public class HelpoutApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(HelpoutApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
