e9316116af098a3d7f07e5958463827b9672beaf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-15 20:38:27 -0300

    Update DolarValue.java

--- src/main/java/tech/noetzold/helpout/model/DolarValue.java
+++ src/main/java/tech/noetzold/helpout/model/DolarValue.java
@@ -1,11 +1,33 @@
 package tech.noetzold.helpout.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class DolarValue {
+    private String code;
+    private String codein;
+    private String name;
+    private BigDecimal high;
+    private BigDecimal low;
+    @JsonProperty("varBid")
+    private BigDecimal variationBid;
+    @JsonProperty("pctChange")
+    private BigDecimal percentChange;
+    private BigDecimal bid;
     private BigDecimal ask;
+    private BigDecimal timestamp;
+    @JsonProperty("create_date")
+    private String createDate;
 
-    public BigDecimal getAsk() {
-        return ask;
-    }
 }
