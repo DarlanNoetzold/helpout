c483d5dfdb3170fb80cb24601d50ce5d55edaf4f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-15 18:41:50 -0300

    Update HelpoutApplication.java

--- src/main/java/tech/noetzold/helpout/HelpoutApplication.java
+++ src/main/java/tech/noetzold/helpout/HelpoutApplication.java
@@ -2,9 +2,10 @@ package tech.noetzold.helpout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {})
+@EnableFeignClients
 public class HelpoutApplication {
 
 	public static void main(String[] args) {
