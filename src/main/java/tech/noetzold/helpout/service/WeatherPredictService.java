8ba65ff8294d9c3c985800c7638646a661c77002
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-15 20:38:41 -0300

    Update WeatherPredictService.java

--- src/main/java/tech/noetzold/helpout/service/WeatherPredictService.java
+++ src/main/java/tech/noetzold/helpout/service/WeatherPredictService.java
@@ -1,7 +1,20 @@
 package tech.noetzold.helpout.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.noetzold.helpout.client.WeatherFeignClient;
+import tech.noetzold.helpout.model.WeatherResponse;
 
 @Service
 public class WeatherPredictService {
+    private final WeatherFeignClient weatherFeignClient;
+
+    @Autowired
+    public WeatherPredictService(WeatherFeignClient weatherFeignClient) {
+        this.weatherFeignClient = weatherFeignClient;
+    }
+
+    public WeatherResponse getWeather(String lat, String lon, String apiKey) {
+        return weatherFeignClient.getWeather(lat, lon, apiKey);
+    }
 }
