70a19b205c22ec2652ee2af66d3da1627dd86b17
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-15 18:42:12 -0300

    Update DolarInformationService.java

--- src/main/java/tech/noetzold/helpout/service/DolarInformationService.java
+++ src/main/java/tech/noetzold/helpout/service/DolarInformationService.java
@@ -1,7 +1,23 @@
 package tech.noetzold.helpout.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.noetzold.helpout.model.DolarResponse;
+import tech.noetzold.helpout.repository.DolarFeignClient;
+
+import java.math.BigDecimal;
 
 @Service
-public class DolarInformationService{
+public class DolarInformationService {
+    private DolarFeignClient dolarFeignClient;
+
+    @Autowired
+    public void dolarService(DolarFeignClient dolarFeignClient) {
+        this.dolarFeignClient = dolarFeignClient;
+    }
+
+    public BigDecimal getDolarRate() {
+        DolarResponse dolarResponse = dolarFeignClient.getDolarRate();
+        return dolarResponse.getUSD().getAsk();
+    }
 }
