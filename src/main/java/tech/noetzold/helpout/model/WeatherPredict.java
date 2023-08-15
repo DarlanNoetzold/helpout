d14a2ca401b53d95148546321cbac1472830ff15
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-15 20:38:29 -0300

    Update WeatherPredict.java

--- src/main/java/tech/noetzold/helpout/model/WeatherPredict.java
+++ src/main/java/tech/noetzold/helpout/model/WeatherPredict.java
@@ -1,4 +1,27 @@
 package tech.noetzold.helpout.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class WeatherPredict {
+
+    private BigDecimal temp;
+    private BigDecimal feels_like;
+    private BigDecimal temp_min;
+    private BigDecimal temp_max;
+    private int pressure;
+    private int humidity;
+    private int sea_level;
+    private int grnd_level;
+
 }
