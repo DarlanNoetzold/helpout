67f29357b3bb0d32be0303bcf7f78d3ee2919944
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-15 18:42:05 -0300

    Update WeatherPredict.java

--- src/main/java/tech/noetzold/helpout/model/WeatherPredict.java
+++ src/main/java/tech/noetzold/helpout/model/WeatherPredict.java
@@ -1,13 +1,4 @@
 package tech.noetzold.helpout.model;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity(name = "weather_predict")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class WeatherPredict {
 }
