da8c7c74cc68c0c797976186ced20b7909ee271d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-17 21:56:18 -0300

    Create TabnewsResponse.java

---
+++ src/main/java/tech/noetzold/helpout/model/TabnewsResponse.java
@@ -0,0 +1,30 @@
+package tech.noetzold.helpout.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class TabnewsResponse {
+    private String id;
+    private String owner_id;
+    private String parent_id;
+    private String slug;
+    private String title;
+    private String status;
+    private String source_url;
+    private String created_at;
+    private String updated_at;
+    private String published_at;
+    private String deleted_at;
+    private Integer tabcoins;
+    private String owner_username;
+    private Integer children_deep_count;
+}
