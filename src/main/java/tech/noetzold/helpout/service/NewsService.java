7161285b78e7110bc4e1a924d3ca96ddb16d5b74
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-17 21:56:22 -0300

    Update NewsService.java

--- src/main/java/tech/noetzold/helpout/service/NewsService.java
+++ src/main/java/tech/noetzold/helpout/service/NewsService.java
@@ -1,7 +1,18 @@
 package tech.noetzold.helpout.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.noetzold.helpout.client.NewsFeignClient;
 
 @Service
 public class NewsService {
+
+    private final NewsFeignClient newsFeignClient;
+
+    @Autowired
+    public NewsService(NewsFeignClient newsFeignClient) {
+        this.newsFeignClient = newsFeignClient;
+    }
+
+
 }
