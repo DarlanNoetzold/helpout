4acafd5bdcd08c5dc21d8980dc3f54a837e98406
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-18 21:54:55 -0300

    Update NewsCommand.java

--- src/main/java/tech/noetzold/helpout/command/NewsCommand.java
+++ src/main/java/tech/noetzold/helpout/command/NewsCommand.java
@@ -1,4 +1,40 @@
 package tech.noetzold.helpout.command;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import tech.noetzold.helpout.service.NewsService;
+
+@ShellComponent
 public class NewsCommand {
+
+    private final NewsService newsService;
+
+    @Autowired
+    public NewsCommand(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+    @ShellMethod("Mostra as notícias.")
+    public void news() {
+        System.out.print("Obtendo notícias   ");
+        animateLoading();
+
+        String news = newsService.getNews();
+
+        System.out.println("\nNoticicias: " + news + " R$");
+    }
+
+    private void animateLoading() {
+        String[] animationFrames = {"|", "/", "-", "\\"};
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(100);
+                System.out.print("\b" + animationFrames[i % animationFrames.length]);
+                System.out.flush();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }
