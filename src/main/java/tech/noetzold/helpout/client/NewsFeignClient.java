b1018d8b1a37ea20e304dcdc5af7432f537780ee
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-18 21:54:53 -0300

    Update NewsFeignClient.java

--- src/main/java/tech/noetzold/helpout/client/NewsFeignClient.java
+++ src/main/java/tech/noetzold/helpout/client/NewsFeignClient.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import tech.noetzold.helpout.model.TabnewsResponse;
 
+import java.util.List;
+
 @FeignClient(name = "news", url = "https://www.tabnews.com.br/api/v1")
 public interface NewsFeignClient {
 
     @GetMapping("/contents")
-    TabnewsResponse getNews(@RequestParam("page") String page, @RequestParam("per_page") String perPage, @RequestParam("strategy") String strategy);
+    List<TabnewsResponse> getNews(@RequestParam("page") String page, @RequestParam("per_page") String perPage, @RequestParam("strategy") String strategy);
 
 }
