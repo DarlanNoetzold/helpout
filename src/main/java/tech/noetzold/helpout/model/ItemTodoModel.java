39beee863142f941e50bf1a000896d167c37d08b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-19 10:30:58 -0300

    Update ItemTodoModel.java

--- src/main/java/tech/noetzold/helpout/model/ItemTodoModel.java
+++ src/main/java/tech/noetzold/helpout/model/ItemTodoModel.java
@@ -1,4 +1,30 @@
 package tech.noetzold.helpout.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import javax.persistence.*;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class ItemTodoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private boolean check;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private CategoryTodoModel category;
+
 }
