b8f252b3222f4d7562938faf8eade55285eafc30
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-19 10:31:01 -0300

    Create CategoryTodoRepository.java

---
+++ src/main/java/tech/noetzold/helpout/repository/CategoryTodoRepository.java
@@ -0,0 +1,6 @@
+package tech.noetzold.helpout.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryTodoRepository extends JpaRepository<CategoryTodoRepository, Long> {
+}
