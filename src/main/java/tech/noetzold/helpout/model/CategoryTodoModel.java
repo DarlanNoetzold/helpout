f47742ca29a19f9d2cbfe8914145a0d701b2fc25
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-19 10:30:56 -0300

    Update CategoryTodoModel.java

--- src/main/java/tech/noetzold/helpout/model/CategoryTodoModel.java
+++ src/main/java/tech/noetzold/helpout/model/CategoryTodoModel.java
@@ -1,4 +1,28 @@
 package tech.noetzold.helpout.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class CategoryTodoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Date date;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<ItemTodoModel> items;
 }
