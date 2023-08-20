e2a1ac4e0b3125bfe991f3e6740556a3cb5a1151
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-19 22:49:27 -0300

    Update CategoryTodoRepository.java

--- src/main/java/tech/noetzold/helpout/repository/CategoryTodoRepository.java
+++ src/main/java/tech/noetzold/helpout/repository/CategoryTodoRepository.java
@@ -1,6 +1,10 @@
 package tech.noetzold.helpout.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tech.noetzold.helpout.model.CategoryTodoModel;
 
-public interface CategoryTodoRepository extends JpaRepository<CategoryTodoRepository, Long> {
+@Repository
+public interface CategoryTodoRepository extends JpaRepository<CategoryTodoModel, Long> {
+
 }
