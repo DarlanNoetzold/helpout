3f452b9f202b182a2672822db66160017f57f768
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-19 22:49:33 -0300

    Update ItemTodoService.java

--- src/main/java/tech/noetzold/helpout/service/ItemTodoService.java
+++ src/main/java/tech/noetzold/helpout/service/ItemTodoService.java
@@ -1,4 +1,14 @@
 package tech.noetzold.helpout.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.noetzold.helpout.repository.ItemTodoRepository;
+
+@Service
 public class ItemTodoService {
+
+    @Autowired
+    ItemTodoRepository itemTodoRepository;
+
+
 }
