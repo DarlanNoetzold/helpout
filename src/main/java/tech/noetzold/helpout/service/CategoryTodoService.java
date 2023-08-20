dfe07e3dbac869c86c1bddfcc4a1f5ca768005aa
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-19 22:49:30 -0300

    Update CategoryTodoService.java

--- src/main/java/tech/noetzold/helpout/service/CategoryTodoService.java
+++ src/main/java/tech/noetzold/helpout/service/CategoryTodoService.java
@@ -1,4 +1,14 @@
 package tech.noetzold.helpout.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.noetzold.helpout.repository.CategoryTodoRepository;
+
+@Service
 public class CategoryTodoService {
+
+    @Autowired
+    CategoryTodoRepository categoryTodoRepository;
+
+
 }
