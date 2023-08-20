abf715829ce7b939400c1e6d43347cb449a2b0cc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-19 22:49:14 -0300

    Update DolarResponse.java

--- src/main/java/tech/noetzold/helpout/model/DolarResponse.java
+++ src/main/java/tech/noetzold/helpout/model/DolarResponse.java
@@ -2,20 +2,22 @@ package tech.noetzold.helpout.model;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Entity
 public class DolarResponse {
 
+    private Long id;
     @JsonProperty("USDBRL")
     private DolarValue USDBRL;
 }
