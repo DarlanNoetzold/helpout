eca1de6a1f7ae96b50ed630122bc2031f1987086
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-19 22:49:25 -0300

    Update WeatherResponse.java

--- src/main/java/tech/noetzold/helpout/model/WeatherResponse.java
+++ src/main/java/tech/noetzold/helpout/model/WeatherResponse.java
@@ -1,17 +1,18 @@
 package tech.noetzold.helpout.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Entity
 public class WeatherResponse {
 
     WeatherPredict main;
