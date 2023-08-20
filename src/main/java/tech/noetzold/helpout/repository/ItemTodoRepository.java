c9ee0b13ee8522724e281dfd076275fe78e45ff9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-19 22:49:29 -0300

    Update ItemTodoRepository.java

--- src/main/java/tech/noetzold/helpout/repository/ItemTodoRepository.java
+++ src/main/java/tech/noetzold/helpout/repository/ItemTodoRepository.java
@@ -1,7 +1,9 @@
 package tech.noetzold.helpout.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.noetzold.helpout.model.ItemTodoModel;
 
+@Repository
 public interface ItemTodoRepository extends JpaRepository<ItemTodoModel, Long> {
 }
