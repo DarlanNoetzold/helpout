d73276d7a2a3961f59f192817738eb87c67e84b3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-21 15:13:07 -0300

    Update CategoryTodoRepository.java

--- src/main/java/tech/noetzold/helpout/repository/CategoryTodoRepository.java
+++ src/main/java/tech/noetzold/helpout/repository/CategoryTodoRepository.java
@@ -1,10 +1,19 @@
 package tech.noetzold.helpout.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tech.noetzold.helpout.model.CategoryTodoModel;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface CategoryTodoRepository extends JpaRepository<CategoryTodoModel, Long> {
 
+    List<CategoryTodoModel> findByName(String name);
+
+    List<CategoryTodoModel> findAllByDate(LocalDate date);
 }
