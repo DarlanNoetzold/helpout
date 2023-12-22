479ef7d97f134eb1f7fe62d86a79953a5507fb3d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-22 08:38:46 -0300

    Update DolarValue.java

--- src/main/java/tech/noetzold/helpout/model/DolarValue.java
+++ src/main/java/tech/noetzold/helpout/model/DolarValue.java
@@ -1,10 +1,6 @@
 package tech.noetzold.helpout.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
